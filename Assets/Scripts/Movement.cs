14490207afa0f388cf2076d81b8ee7c765552a0e
Gianmarco Biscini <g.biscini@gmail.com>
2020-03-14 14:59:38 +0100

    movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float speed = 1.0f;
+    private bool moveRight=false;
+    private bool moveLeft=false;
+    private Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+            transform.position -= Vector3.right * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D) || (moveRight && !moveLeft))
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W))
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S) || (moveRight && !moveLeft))
+            transform.position -= Vector3.right * speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ////TODO
+        }
+    }
+
+   
+}
