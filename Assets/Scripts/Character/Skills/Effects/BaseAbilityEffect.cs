5b992f9e761c63f2f98c637e000bba8e91198241
Gianmarco Biscini <g.biscini@gmail.com>
2020-03-14 14:53:09 +0100

    delete all

--- Assets/Scripts/Character/Skills/Effects/BaseAbilityEffect.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BaseAbilityEffect : MonoBehaviour{
-    public abstract void apply(GameObject target);
-}
--- Assets/Scripts/Items/Tradeable.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Tradeable : MonoBehaviour{
-    public int sellPrice { get; private set; }
-    public int buyPrice { get; private set; }
-}
