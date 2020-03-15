093bfd3295b0f0bf5583dce896296f46fa80f1fc
Xe913 <simonegrossi@gmail.com>
2020-03-15 19:01:23 +0100

    Add screenfade

---
+++ Assets/FadeInOut.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInOut : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+
+    public void FadeOut()
+    {
+        Debug.Log("FADE");
+        anim.SetTrigger("FadeToBlack");
+    }
+}
