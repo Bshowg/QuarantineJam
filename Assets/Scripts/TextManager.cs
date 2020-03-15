14f5aaf12654a99d4b2aa2c5ee622bdb1fec8488
fedebamba <federicobambagioni@gmail.com>
2020-03-15 11:30:27 -0700

    s

--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextManager : MonoBehaviour{
     public GameObject text;
+    public List<string> possibleLines;
 
     public void requireTexts(List<GameObject> subjects) {
         foreach (GameObject g in subjects) {
@@ -12,6 +14,14 @@ public class TextManager : MonoBehaviour{
             GameObject newText = GameObject.Instantiate(text) as GameObject;
             newText.transform.SetParent(transform);
             newText.GetComponent<WigglyWaggly>().subject = g;
+            if (possibleLines != null) {
+                if (possibleLines.Count > 0) {
+                    int l = Random.Range(0, possibleLines.Count);
+                    string s = possibleLines[l];
+                    newText.GetComponent<Text>().text = s;
+                }
+            }
+
         }
     
     }
