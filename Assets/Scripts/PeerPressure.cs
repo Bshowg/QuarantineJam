15c9b45cfc9305ef9213f1657511706456f0d3e4
fedebamba <federicobambagioni@gmail.com>
2020-03-15 09:48:31 -0700

    d

---
+++ Assets/Scripts/PeerPressure.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeerPressure : MonoBehaviour{
+    public float radius;
+    
+    
+    
+    private int crowdLayer = 1 << LayerMask.NameToLayer("Crowd");
+
+    private bool continueChecking = true;
+
+    private IEnumerator checkForPeopleAround() {
+        while (continueChecking) {
+            Collider2D[] cs = Physics2D.OverlapCircleAll(transform.position, radius, crowdLayer);
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+
+    private void requireTexts(int howMany) {
+        Debug.Log("Required " + howMany + " texts");
+
+    }
+
+    void Start(){
+        StartCoroutine(checkForPeopleAround());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
