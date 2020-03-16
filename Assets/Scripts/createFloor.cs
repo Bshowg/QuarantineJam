6a34f2a77cb81f60c32a67b17fe9643c3c40ca01
Gianmarco Biscini <g.biscini@gmail.com>
2020-03-16 20:36:52 +0100

    pavimento

---
+++ Assets/Scripts/createFloor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createFloor : MonoBehaviour
+{
+    public GameObject tile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = -60; i < 60; i++)
+        {
+            for (int j = -30; j < 33; j++)
+            {
+                var t = Instantiate(tile, this.transform);
+                t.transform.position = new Vector3(i, j, -1f);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
