dccd99fcb7604421ac4adcd514e21b806652bedb
Gianmarco Biscini <g.biscini@gmail.com>
2020-03-16 21:12:47 +0100

    intro and final

--- Assets/Scripts/createFloor.cs
+++ Assets/Scripts/createFloor.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class createFloor : MonoBehaviour
 {
+    public int x_pos = 20;
+    public int x_neg = -20;
+    public int y_pos = 11;
+    public int y_neg = -10;
     public GameObject tile;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = -20; i < 20; i++)
+        for (int i = x_neg; i < x_pos; i++)
         {
-            for (int j = -10; j < 11; j++)
+            for (int j = y_neg; j < y_pos; j++)
             {
                 var t = Instantiate(tile, this.transform);
                 t.transform.position = new Vector3(i*3, j*3, -1f);
