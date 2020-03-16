47d7c16446ef990ebf3c24cd49a118bfeec7cc9e
fedebamba <federicobambagioni@gmail.com>
2020-03-16 07:31:34 -0700

    d

---
+++ Assets/Scripts/Pause/PauseButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour{
+    private GameObject panel;
+
+    // Start is called before the first frame update
+    void Start(){
+        panel = GameObject.FindGameObjectWithTag("PausePanel");
+        panel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (!panel.activeSelf){
+                panel.SetActive(true);
+                panel.GetComponent<PauseManager>().activate();
+            }
+            else {
+                panel.GetComponent<PauseManager>().deactivate();
+                panel.SetActive(false);
+            }
+            
+        }      
+    }
+}
---
+++ Assets/Scripts/Pause/PauseManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void activate() {
+        Time.timeScale = 0f;
+    }
+
+    public void deactivate() {
+        Time.timeScale = 1f;
+        gameObject.SetActive(false);
+    }
+    public void OnDestroy()
+    {
+        Time.timeScale = 1f;   
+    }
+
+    public void exit(){
+        Application.Quit();
+    }
+}
